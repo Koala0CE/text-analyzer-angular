c4c714174b6920f46a8adad925f6a09ab6a062d2
Koala0CE <reactor.koala0c@icloud.com>
2024-07-23 11:35:13 +0200

    chore: Add UpperCasePipe import to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TextAnalyzerComponent } from './text-analyzer/text-analyzer.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TextAnalyzerComponent, MatToolbarModule],
+  imports: [
+    RouterOutlet,
+    TextAnalyzerComponent,
+    MatToolbarModule,
+    UpperCasePipe,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
