f8d7028f72ce36558831f2ae95871607b92410da
Koala0CE <reactor.koala0c@icloud.com>
2024-07-23 23:00:55 +0200

    unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, HttpClientTestingModule, BrowserAnimationsModule],
     }).compileComponents();
   });
 
@@ -24,6 +26,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, text-analyzer-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Text Analyzer App'
+    );
   });
 });
